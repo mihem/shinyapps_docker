16506537920e2eff99333d6c64b3a41f203be2ef
mischko <mhem@mailbox.org>
2025-11-16 11:20:57 +0100

    update docker run version

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run \
 	--detach \
-	--name shinyapps_v8 \
+	--name shinyapps_v9 \
 	--restart=unless-stopped \
 	-p 3838:3838 \
-	mihem/shinyapps_3838:v8.0
+	mihem/shinyapps_3838:v9.0
