f3507b0aae48a654a4162a739b4c05913b3039b0
mischko <mhem@mailbox.org>
2026-03-10 22:29:53 +0100

    bump to v10

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run \
 	--detach \
-	--name shinyapps_v9 \
+	--name shinyapps_v10 \
 	--restart=unless-stopped \
 	-p 3838:3838 \
-	mihem/shinyapps_3838:v9.0
+	mihem/shinyapps_3838:v10.0
